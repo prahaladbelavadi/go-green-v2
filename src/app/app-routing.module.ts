21d993d758bdb2725a77229e301fb1e06a0d5963
Prahalad Belavadi <prahalad@belavadi.com>
2019-06-22 22:22:38 +0530

    generate page4
    Signed-off-by: Prahalad Belavadi <prahalad@belavadi.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,7 +16,8 @@ const routes: Routes = [
     loadChildren: './list/list.module#ListPageModule'
   },
   { path: 'page2', loadChildren: './page2/page2.module#Page2PageModule' },
-  { path: 'page3', loadChildren: './page3/page3.module#Page3PageModule' }
+  { path: 'page3', loadChildren: './page3/page3.module#Page3PageModule' },
+  { path: 'page4', loadChildren: './page4/page4.module#Page4PageModule' }
 ];
 
 @NgModule({
