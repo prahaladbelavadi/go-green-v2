47677ad68fc0e5e7fce31d6e6d0a50a5c2d32f0d
Prahalad Belavadi <prahalad@belavadi.com>
2019-06-22 22:47:48 +0530

    page5 finished and page 6 generated
    Signed-off-by: Prahalad Belavadi <prahalad@belavadi.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,8 @@ const routes: Routes = [
   { path: 'page2', loadChildren: './page2/page2.module#Page2PageModule' },
   { path: 'page3', loadChildren: './page3/page3.module#Page3PageModule' },
   { path: 'page4', loadChildren: './page4/page4.module#Page4PageModule' },
-  { path: 'page5', loadChildren: './page5/page5.module#Page5PageModule' }
+  { path: 'page5', loadChildren: './page5/page5.module#Page5PageModule' },
+  { path: 'page6', loadChildren: './page6/page6.module#Page6PageModule' }
 ];
 
 @NgModule({
